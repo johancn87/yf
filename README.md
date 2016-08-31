341562f4517bfbe43247d9493abb7d7c265136f3
johancn <johancn87@gmail.com>
2016-08-31 19:58:56 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# yfinance [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> Yahoo Finance API for NodeJs.
+
+## Installation
+
+```sh
+$ npm install --save yfinance
+```
+
+## Usage
+
+```js
+var yfinance = require('yfinance');
+
+yfinance('Rainbow');
+```
+## License
+
+MIT © [Johan Carlsson]()
+
+
+[npm-image]: https://badge.fury.io/js/yfinance.svg
+[npm-url]: https://npmjs.org/package/yfinance
+[travis-image]: https://travis-ci.org/johancn87/yfinance.svg?branch=master
+[travis-url]: https://travis-ci.org/johancn87/yfinance
+[daviddm-image]: https://david-dm.org/johancn87/yfinance.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/johancn87/yfinance
+[coveralls-image]: https://coveralls.io/repos/johancn87/yfinance/badge.svg
+[coveralls-url]: https://coveralls.io/r/johancn87/yfinance
---
+++ test/index.js
@@ -0,0 +1,124 @@
+//import assert from 'assert';
+import yfinance from '../lib';
+
+describe('yfinance', function () {
+    describe('getQuotes', function () {
+        it('Single quote!', function (done) {
+            yfinance.getQuotes('JNJ', function (data) {
+                if (!data) {
+                    done('Got no data!');
+                    return;
+                }
+                if (data[0].Symbol !== 'JNJ') {
+                    done('Incorrect symbol!');
+                    return;
+                }
+                if (!data[0].DaysHigh) {
+                    done('No quote data!');
+                    return;
+                }
+                done();
+            });
+        });
+
+        it('Comma seperated quotes!', function (done) {
+            yfinance.getQuotes('JNJ,GOOG', function (data) {
+                if (!data) {
+                    done('Got no data!');
+                    return;
+                }
+                if (!Array.isArray(data)) {
+                    done('Did not return array!');
+                    return;
+                }
+                if (data[0].Symbol !== 'JNJ' && data[1].Symbol !== 'JNJ') {
+                    done('Missing symbol JNJ!');
+                    return;
+                }
+                if (data[0].Symbol !== 'GOOG' && data[1].Symbol !== 'GOOG') {
+                    done('Missing symbol GOOG!');
+                    return;
+                }
+                if (!data[0].DaysHigh || !data[1].DaysHigh) {
+                    done('No quote data!');
+                    return;
+                }
+                done();
+            });
+        });
+
+        it('Array of quotes!', function (done) {
+            yfinance.getQuotes(['JNJ', 'GOOG'], function (data) {
+                if (!data) {
+                    done('Got no data!');
+                    return;
+                }
+                if (!Array.isArray(data)) {
+                    done('Did not return array!');
+                    return;
+                }
+                if (data[0].Symbol !== 'JNJ' && data[1].Symbol !== 'JNJ') {
+                    done('Missing symbol JNJ!');
+                    return;
+                }
+                if (data[0].Symbol !== 'GOOG' && data[1].Symbol !== 'GOOG') {
+                    done('Missing symbol GOOG!');
+                    return;
+                }
+                if (!data[0].DaysHigh || !data[1].DaysHigh) {
+                    done('No quote data!');
+                    return;
+                }
+                done();
+            });
+        });
+    });
+
+    describe('getHistorical', function () {
+        it('Should get some results!', function (done) {
+            yfinance.getHistorical('JNJ', '2016-08-01', '2016-08-05', function (data) {
+                if (!data) {
+                    done('Got no data!');
+                    return;
+                }
+                if (data.length !== 5) {
+                    done('Incorrect number of results!');
+                    return;
+                }
+                if (data[1].Symbol !== 'JNJ') {
+                    done('Incorrect symbol!');
+                    return;
+                }
+                if (!data[1].Close) {
+                    done('No quote data!');
+                    return;
+                }
+                done();
+            });
+        });
+    });
+
+    describe('getDividendHistory', function () {
+        it('Should get some results!', function (done) {
+            yfinance.getDividendHistory('JNJ', '2015-01-01', '2015-12-31', function (data) {
+                if (!data) {
+                    done('Got no data!');
+                    return;
+                }
+                if (data.length !== 7) {
+                    done('Incorrect number of results!');
+                    return;
+                }
+                if (data[1].Symbol !== 'JNJ') {
+                    done('Incorrect symbol!');
+                    return;
+                }
+                if (!data[1].Dividends) {
+                    done('No Dividends data!');
+                    return;
+                }
+                done();
+            });
+        });
+    });
+});
